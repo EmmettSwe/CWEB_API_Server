7d95627989d07393c7f75093aaae00a94c07aaf3
Swejda9940 <Swejda9940@saskpolytech.ca>
2024-11-05 10:21:08 -0600

    Another setup commit

--- src/data-source.ts
+++ src/data-source.ts
@@ -3,7 +3,8 @@ import { DataSource } from "typeorm"
 import { User } from "./entity/User"
 
 export const AppDataSource = new DataSource({
-    
+    type: 'sqlite', // prostgress, localdb, mysql, mariadb
+    database: './sqlite.db',
     synchronize: true,
     logging: false,
     entities: [User],
